ec00f9a2113c0dff8b774f331dd6210d6e441669
江毅 <903199067@qq.com>
2020-10-16 11:28:03 +0800

    multi handler test

--- DotNetCore.DDD.Template.Application/DomainEventHandlers/TestDomainEventHandler.cs
+++ DotNetCore.DDD.Template.Application/DomainEventHandlers/TestDomainEventHandler.cs
@@ -4,6 +4,9 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 当一个领域事件有多个处理器时，是按代码顺序依次执行的
+/// </summary>
 namespace DotNetCore.DDD.Template.Application.DomainEventHandlers
 {
     public class TestDomainEventHandler : INotificationHandler<TestDomainEvent>
@@ -13,4 +16,13 @@ namespace DotNetCore.DDD.Template.Application.DomainEventHandlers
             Console.WriteLine(string.Format("Name:{0},Sex:{1}", notification.Name, notification.Sex));
         }
     }
+
+    public class TestDomainEventHandler2 : INotificationHandler<TestDomainEvent>
+    {
+        public async Task Handle(TestDomainEvent notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(string.Format("Name2:{0},Sex2:{1}", notification.Name, notification.Sex));
+        }
+    }
+
 }
--- DotNetCore.DDD.Template.Application/QueryHandlers/TestQueryHandler.cs
+++ DotNetCore.DDD.Template.Application/QueryHandlers/TestQueryHandler.cs
@@ -1,8 +1,12 @@
 ﻿using DotNetCore.DDD.Template.Domain.DomainEvents;
 using MediatR;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 当一个query或者command有多个处理器时，只执行第一个处理器
+/// </summary>
 namespace DotNetCore.DDD.Template.Application.QueryHandlers
 {
     public class TestQueryHandler : IRequestHandler<TestQueryInput, TestQueryOutput>
@@ -18,7 +22,25 @@ namespace DotNetCore.DDD.Template.Application.QueryHandlers
             //todo business
 
             await _mediator.Publish(new TestDomainEvent("江毅", "男"));
+            Console.WriteLine("this is TestQueryHandler");
+            return new TestQueryOutput { Name = request.Name };
+        }
+    }
 
+    public class TestQueryHandler2 : IRequestHandler<TestQueryInput, TestQueryOutput>
+    {
+        private readonly IMediator _mediator;
+
+        public TestQueryHandler2(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<TestQueryOutput> Handle(TestQueryInput request, CancellationToken cancellationToken)
+        {
+            //todo business
+
+            await _mediator.Publish(new TestDomainEvent("江毅2", "男2"));
+            Console.WriteLine("this is TestQueryHandler2");
             return new TestQueryOutput { Name = request.Name };
         }
     }
